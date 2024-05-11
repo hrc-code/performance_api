742b2a9c89fc11db915939f2d2492a9f3f993e7e
xxx <1169007205@qq.com>
2024-05-11 23:35:40 +0800

    master

--- src/main/java/com/example/workflow/controller/PreviewController.java
+++ src/main/java/com/example/workflow/controller/PreviewController.java
@@ -8,9 +8,9 @@ import com.example.workflow.entity.*;
 import com.example.workflow.mapper.PositionKpiViewMapper;
 import com.example.workflow.mapper.PositionPieceViewMapper;
 import com.example.workflow.mapper.PositionScoreViewMapper;
-import com.example.workflow.service.KpiRuleService;
-import com.example.workflow.service.PieceRuleService;
-import com.example.workflow.service.ScoreRuleService;
+import com.example.workflow.mapper.RegionCoefficientMapper;
+import com.example.workflow.service.*;
+import com.example.workflow.vo.PositionAssessorView;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,9 +36,15 @@ public class PreviewController {
     @Autowired
             private PositionPieceViewMapper PositionPieceViewMapper;
     @Autowired
-            private KpiRuleService KpiRuleService;
+            private KpiRulePercentService KpiRulePercentService;
     @Autowired
             private PositionKpiViewMapper PositionKpiViewMapper;
+    @Autowired
+            private RegionCoefficientMapper RegionCoefficientMapper;
+    @Autowired
+            private CoefficientViewService CoefficientViewService;
+    @Autowired
+            private PositionAssessorViewService PositionAssessorViewService;
     LocalDate today = LocalDate.now();
     LocalDateTime beginTime = LocalDateTime.of(today.withDayOfMonth(1).minusMonths(1), LocalTime.MIN);
     LocalDateTime endTime = LocalDateTime.of(today.withDayOfMonth(today.lengthOfMonth()).minusMonths(1), LocalTime.MAX);
@@ -98,10 +104,12 @@ public class PreviewController {
     }
 
     @PostMapping("/getKpiRuleList")
-    private R<List<KpiRule>> getKpiRuleList(){
+    private R<List<KpiRulePercent>> getKpiRuleList(){
 
-        List<KpiRule> list=KpiRuleService.lambdaQuery()
-                .eq(KpiRule::getState,1)
+        List<KpiRulePercent> list=KpiRulePercentService.lambdaQuery()
+                .eq(KpiRulePercent::getState,1)
+                .orderByAsc(KpiRulePercent::getId)
+                .orderByAsc(KpiRulePercent::getKpiKey)
                 .apply(StringUtils.checkValNotNull(beginTime),
                         "date_format (create_time,'%Y-%m-%d %H:%i:%s') >= date_format ({0},'%Y-%m-%d %H:%i:%s')", beginTime)
                 .apply(StringUtils.checkValNotNull(endTime),
@@ -121,6 +129,48 @@ public class PreviewController {
                 .apply(StringUtils.checkValNotNull(endTime),
                         "date_format (create_time,'%Y-%m-%d %H:%i:%s') <= date_format ({0},'%Y-%m-%d %H:%i:%s')", endTime);;
         List<PositionKpiView> list=PositionKpiViewMapper.selectList(queryWrapper);
+
+        return R.success(list);
+    }
+
+    @PostMapping("/getRegionCoefficientList")
+    private R<List<RegionCoefficient>> getRegionCoefficientList(){
+        LambdaQueryWrapper<RegionCoefficient> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.orderByAsc(RegionCoefficient::getId)
+                .eq(RegionCoefficient::getState,1)
+                .apply(StringUtils.checkValNotNull(beginTime),
+                        "date_format (create_time,'%Y-%m-%d %H:%i:%s') >= date_format ({0},'%Y-%m-%d %H:%i:%s')", beginTime)
+                .apply(StringUtils.checkValNotNull(endTime),
+                        "date_format (create_time,'%Y-%m-%d %H:%i:%s') <= date_format ({0},'%Y-%m-%d %H:%i:%s')", endTime);;
+        List<RegionCoefficient> list=RegionCoefficientMapper.selectList(queryWrapper);
+
+        return R.success(list);
+    }
+
+    @PostMapping("/getEmpCoefficientList")
+    private R<List<CoefficientView>> getEmpCoefficientList(){
+
+        List<CoefficientView> list=CoefficientViewService.lambdaQuery()
+                .apply(StringUtils.checkValNotNull(beginTime),
+                        "date_format (create_time,'%Y-%m-%d %H:%i:%s') >= date_format ({0},'%Y-%m-%d %H:%i:%s')", beginTime)
+                .apply(StringUtils.checkValNotNull(endTime),
+                        "date_format (create_time,'%Y-%m-%d %H:%i:%s') <= date_format ({0},'%Y-%m-%d %H:%i:%s')", endTime)
+                .orderByDesc(CoefficientView::getEmpId)
+                .list();
+
+        return R.success(list);
+    }
+
+    @PostMapping("/getPositionAssessorView")
+    private R<List<PositionAssessorView>> getPositionAssessorView(){
+
+        List<PositionAssessorView> list=PositionAssessorViewService.lambdaQuery()
+                .eq(PositionAssessorView::getState,1)
+                .apply(StringUtils.checkValNotNull(beginTime),
+                        "date_format (create_time,'%Y-%m-%d %H:%i:%s') >= date_format ({0},'%Y-%m-%d %H:%i:%s')", beginTime)
+                .apply(StringUtils.checkValNotNull(endTime),
+                        "date_format (create_time,'%Y-%m-%d %H:%i:%s') <= date_format ({0},'%Y-%m-%d %H:%i:%s')", endTime)
+                .list();
 
         return R.success(list);
     }
