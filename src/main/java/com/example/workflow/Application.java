ca8f3743d14e94a130ab5bbe171bf00d0bd90664
hrc <huthrc@qq.com>
2024-11-07 16:41:06 +0800

    refactor:添加启动图标

--- src/main/java/com/example/workflow/Application.java
+++ src/main/java/com/example/workflow/Application.java
@@ -17,6 +17,15 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class Application {
   public static void main(String... args) {
     SpringApplication.run(Application.class, args);
+    // http://patorjk.com/software/taag/?spm=5176.28103460.0.0.297c5d27o3B1lK 横幅内容生成
+    System.out.println("                      _____                                                \n" +
+            "______   ____________/ ____\\___________  _____ _____    ____   ____  ____  \n" +
+            "\\____ \\_/ __ \\_  __ \\   __\\/  _ \\_  __ \\/     \\\\__  \\  /    \\_/ ___\\/ __ \\ \n" +
+            "|  |_> >  ___/|  | \\/|  | (  <_> )  | \\/  Y Y  \\/ __ \\|   |  \\  \\__\\  ___/ \n" +
+            "|   __/ \\___  >__|   |__|  \\____/|__|  |__|_|  (____  /___|  /\\___  >___  >\n" +
+            "|__|        \\/                               \\/     \\/     \\/     \\/    \\/" + '\n'
+            + "绩效管理系统后台服务启动成功");
+
   }
 
 }
