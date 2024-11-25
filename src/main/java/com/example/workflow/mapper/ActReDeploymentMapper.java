aba2c09dbbec9f095f778f1f9fbffdbe6e270740
hrc <huthrc@qq.com>
2024-11-25 18:56:54 +0800

    delete: 无用类

--- src/main/java/com/example/workflow/mapper/ActReDeploymentMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.workflow.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.workflow.model.entity.ActReDeployment;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ActReDeploymentMapper extends BaseMapper<ActReDeployment> {
-}
--- src/main/java/com/example/workflow/repository/PdfFileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.workflow.repository;
-
-import com.example.workflow.model.entity.PdfFile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PdfFileRepository extends JpaRepository<PdfFile, Long> {
-}
